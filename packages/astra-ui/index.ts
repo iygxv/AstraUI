67d90e6cc105a23214a91d1cf76bb961a4fdb582
zwf <871772825@qq.com>
2023-05-19 16:08:49 +0800

    fix：修改gitgnore

--- packages/astra-ui/index.ts
+++ packages/astra-ui/index.ts
@@ -3,7 +3,6 @@ import '@astra/style'
 import Button from '@astra/components/button'
 
 const components = [Button]
-
 const install = (app: App): void => {
   components.forEach((component) => {
     app.component(component.name, component)
